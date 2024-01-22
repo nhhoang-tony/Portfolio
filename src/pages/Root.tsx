5071fb6d0edeb933534d96cf7e779cca94bf5d52
Tony Nguyen <t.nguyen@vbreathe.com>
2024-01-23 00:10:17 +1000

    Add Vercel analytics

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import ScrollToTop from '../components/ScrollToTop';
+import { Analytics } from '@vercel/analytics/react';
 
 function RootLayout() {
   return (
@@ -9,6 +10,7 @@ function RootLayout() {
       <main>
         <Outlet></Outlet>
         <ScrollToTop></ScrollToTop>
+        <Analytics></Analytics>
       </main>
     </>
   );
